b189a51b631dde3897c60bfaa4f9eb95a2fd5460
Swechha <swechha16.05@gmail.com>
2019-05-04 09:04:41 -0400

    added trainer and generate

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ import torch.nn.functional as F
 from models.models import UnconditionalLSTM
 from utils import plot_stroke
 from utils.dataset import HandwritingDataset
-from utils.model_utils import compute_unconditional_loss
+from utils.model_utils import compute_unconditional_loss, stable_softmax
 from utils.data_utils import get_data_and_mask, get_inputs_and_targets, train_offset_normalization, valid_offset_normalization, data_denormalization
 
 
