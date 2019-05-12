8998b64854f4826e7f520faef67f6da81d63a557
Swechha <swechha16.05@gmail.com>
2019-05-11 22:57:40 -0400

    fix a bug

--- generate.py
+++ generate.py
@@ -142,7 +142,7 @@ def generate_conditional_sequence(model_path, char_seq, device, char_to_id):
     print("Generating sequence....")
     with torch.no_grad():
         while not model.EOS or seq_len <= 600:
-
+            print(seq_len)
             y_hat, state = model.forward(inp, text, text_mask, initial_hidden, window_vector, kappa)
 
             _hidden = torch.cat([s[0] for s in state], dim=0)
